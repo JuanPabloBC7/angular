ea9e9fedd075654c75abda1c842d99e46c48015e
JuanPabloBC7 <juanpablobc7@gmail.com>
2025-08-04 21:05:32 -0600

    Install primeicons and initial menu bar, clean code.

--- src/app/components/commons/navbar/navbar.component.ts
+++ src/app/components/commons/navbar/navbar.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, Menubar],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  items1: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items1 = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/',
+        badge: '3',
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+        url: 'https://jpbalan7.onrender.com/#contact-us'
+      },
+    ]
+  }
 }
