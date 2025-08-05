d731ef1a3b207708de4255d45d5820d1365371cd
JuanPabloBC7 <juanpablobc7@gmail.com>
2025-08-04 18:45:11 -0600

    Create new component for project schema and install @primeuix/themes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './components/layouts/principal/principal.component';
+import { CrudComponent } from './components/pages/crud/crud.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: PrincipalComponent,
+    children: [
+      {
+        path: '',
+        component: CrudComponent
+      },
+    ]
+  },
+];
---
+++ src/app/components/layouts/principal/principal.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../commons/navbar/navbar.component";
+import { SidebarComponent } from '../../commons/sidebar/sidebar.component';
+import { FooterComponent } from '../../commons/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-principal',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    RouterOutlet,
+],
+  templateUrl: './principal.component.html',
+  styleUrl: './principal.component.scss'
+})
+export class PrincipalComponent {
+
+}
